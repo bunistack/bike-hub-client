390cadc6eb882b256c316c51e9744303ed75f0c2
Herbert Amukhuma <herbertamukhuma6@gmail.com>
2020-05-16 16:08:08 +0300

    project setup

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 
 #include <QQmlApplicationEngine>
 
-// uncomment this line to add the Live Client Module and use live reloading with your custom C++ code
-//#include <FelgoLiveClient>
-
-
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
